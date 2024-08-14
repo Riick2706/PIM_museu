d990c5336c8c3ed774e004e1faf37cbafc47d65a
riick2706 <richardgoncalvesdossantos@gmail.com>
2024-08-14 02:01:18 -0300

    test: update vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import TesteComponente from './screens/testeComponente';
@@ -6,12 +5,10 @@ import './styles/base.css'
 
 import ArteModerna from './screens/arteModerna';
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <BrowserRouter>
       <Routes>
-          <Route path='arte' element={<ArteModerna />} />
+          <Route path='/artes' element={<ArteModerna />} />
           <Route path="teste" element={<TesteComponente />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
 );
